1d1e76cc2971ac64c1c278b5829785eda6d3aac4
Alexander Aldridge <alexander@aldridge.ca>
2021-11-18 21:20:11 -0800

    Added lines_of_code.csv and bugfixes to count_loc.py

--- src/main/python/lines_of_code/count_loc.py
+++ src/main/python/lines_of_code/count_loc.py
@@ -1,3 +1,9 @@
+# Make sure that cloc is installed on your computer before running this code.
+# Change the directory variable to be the directory containing all the subdirectories of repos.
+# This code runs cloc on all directories, and compiles the resulting Java lines of code into output.csv
+# Written for Windows 10. Adjustments may be needed for Linux.
+# Created by Alexander Aldridge as a tool for Fall 2021 SENG 480B at UVic
+
 import subprocess
 import os
 import re
@@ -6,17 +12,28 @@ directory = '.\\code_for_parsing'
 
 
 lines_of_code = ['folder,loc\n']
-for x in list(os.walk(directory))[1:]:
+directories = list(os.walk(directory))
+
+first_directory = 900
+
+for x in directories[first_directory:-1]:
+	if x is None or x[0] is None:
+		print('Error with directory ' + str(x))
+		continue
 	folder = x[0]
 	print('Analysing files in {}'.format(folder))
 	result = subprocess.run(['cloc', folder],
            stdout=subprocess.PIPE,
            stderr=subprocess.STDOUT,
 		   shell=True)
+	match_object = re.search(r'Java\s+\d+\s+\d+\s+\d+', str(result.stdout))
 	
-	javaMatch = re.search(r'Java\s+\d+\s+\d+\s+\d+', str(result.stdout))[0]
-	loc = int(re.findall(r'\d+', javaMatch)[-1])
+	if match_object is None:
+		print('Unable to find match for directory ' + str(directory))
+		continue
+
+	loc = int(re.findall(r'\d+', match_object[0])[-1])
 	lines_of_code.append('{},{}\n'.format(folder, loc))
 
-with open('output.csv', 'w') as out:
+with open('output{}-{}.csv'.format(first_directory, 'end'), 'w') as out:
 	out.writelines(lines_of_code)
